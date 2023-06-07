6e76fbcb12a72d43768563b84e0256d4b004a011
rshezarr <rshezarr@gmail.com>
2023-06-07 12:28:41 +0600

    set: config and bot init function

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/BurntSushi/toml"
+	"log"
+	"tgbot/cmd/bot"
+)
 
+type Config struct {
+	Env      string
+	BotToken string
+}
+
+func main() {
+	configPath := flag.String("config", "config/config.toml", "Path to config file")
+	flag.Parse()
+
+	cfg := &Config{}
+
+	_, err := toml.DecodeFile(*configPath, cfg)
+	if err != nil {
+		log.Fatalf("Error occured while decoding config file: %v", err)
+	}
+
+	upgradeBot := bot.UpgradeBot{
+		Bot: bot.InitBot(cfg.BotToken),
+	}
+
+	upgradeBot.Bot.Handle("/start", bot.StartBotHandler)
+
+	upgradeBot.Bot.Start()
 }
