4ecf4e742a72087b2470e86686842ce08e1b73cc
rshezarr <rshezarr@gmail.com>
2023-06-07 12:26:41 +0600

    add: bot init function and start handler

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -1,1 +1,29 @@
 package bot
+
+import (
+	"gopkg.in/telebot.v3"
+	"log"
+	"time"
+)
+
+type UpgradeBot struct {
+	Bot *telebot.Bot
+}
+
+func InitBot(token string) *telebot.Bot {
+	pref := telebot.Settings{
+		Token:  token,
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := telebot.NewBot(pref)
+	if err != nil {
+		log.Fatalf("Error while initializing bot: %v", err)
+	}
+
+	return b
+}
+
+func StartBotHandler(context telebot.Context) error {
+	return context.Send("Hello, " + context.Sender().FirstName)
+}
